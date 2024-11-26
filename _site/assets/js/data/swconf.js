2b58e4cb83a79a089bb6f0621ee9519d738adb08
wanjwoo <116162460+wanjinwoo@users.noreply.github.com>
2024-11-26 14:18:43 +0000

    feat:init

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1732630526',resources: [
+      '/wsquare5.github.io/assets/css/jekyll-theme-chirpy.css',
+      '/wsquare5.github.io/',
+      
+        '/wsquare5.github.io/categories/',
+      
+        '/wsquare5.github.io/tags/',
+      
+        '/wsquare5.github.io/archives/',
+      
+        '/wsquare5.github.io/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
