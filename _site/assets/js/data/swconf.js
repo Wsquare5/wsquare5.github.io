cd7190cd6bbc0ad6b8c4eacfc0fb248959ff5572
wanjwoo <116162460+wanjinwoo@users.noreply.github.com>
2024-11-27 22:20:30 +0800

    feat:init

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732715844',resources: [
+    cacheName: 'chirpy-1732716710',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
